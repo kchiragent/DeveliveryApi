1e35b539cd1224b8d858a0533e0e82b20a966a8c
kchiragent <kchirag@gmail.com>
2017-12-05 17:07:38 -0800

    Initial Commit

---
+++ src/main/java/com/entmobi/nodems/DeveliveryApiApplication.java
@@ -0,0 +1,12 @@
+package com.entmobi.nodems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveliveryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveliveryApiApplication.class, args);
+	}
+}
